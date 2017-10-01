4fb209eed36060330895930b7c11258821405a17
AlexGomesDS <Alexsantogomes@gmail.com>
2017-10-01 20:17:59 +0100

    [UPD] - Added a simple scatter plot

--- homicides.py
+++ homicides.py
@@ -8,6 +8,8 @@ This is a temporary script file.
 #import lib
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib qt5
 
 #%% import dataset
 
@@ -15,3 +17,9 @@ df=pd.read_csv("database.csv")
 
 #%% 
 df.head()
+df.describe()
+
+#%% 
+df2=df.loc[1:10000]
+plt.figure()
+plt.scatter(df2['Victim Age'].values, df2['Perpetrator Age'].values)
