9ef3cff7bffa30332413c3c841467eaab963ae9c
sssusana <siss.susana@gmail.com>
2017-10-01 20:17:32 +0100

    [UPD] - Added to-do list and some descriptive stats

--- homicides.py
+++ homicides.py
@@ -5,13 +5,33 @@ Spyder Editor
 This is a temporary script file.
 """
 
+## 1st descriptive statistics 
+## 2nd describe main differences between states
+    # 3 plus
+
+## crime profiling
+    #correlations between 
+
+
+
 #import lib
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.plotly as py
 
 #%% import dataset
 
 df=pd.read_csv("database.csv")
 
-#%% 
+#%%
+df.columns 
 df.head()
+df.describe()
+
+
+dfcrimetype = df['Crime Type']
+dfcrimetype.
+plt.hist(dfcrimetype)
+plt.xlabel("Index")
+plt.ylabel("Crime Type")
