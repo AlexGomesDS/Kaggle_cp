13331960268f8b8f5cbca0e5962f8bdb7537cfbc
AlexGomesDS <Alexsantogomes@gmail.com>
2017-10-01 19:35:28 +0100

    [ADD] - Added python script

---
+++ homicides.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#import lib
+import numpy as np
+import pandas as pd
+
+#%% import dataset
+
+df=pd.read_csv("database.csv")
+
+#%% 
+df.head()
