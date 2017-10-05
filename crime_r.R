2614cc5d2e8b943781cc8b44e186b2aca7a19f28
sssusana <siss.susana@gmail.com>
2017-10-05 15:21:52 +0100

    [UPD]

--- crime_r.R
+++ crime_r.R
@@ -1,3 +1,5 @@
+#Process of subsecting data, based on values of 3 variables:
+
 crimeds <- database[which(database$'Victim Count' > 0),]
 teste2 <- crimeds[which(crimeds$`Perpetrator Sex`>4)]
 teste2 <- crimeds[which(crimeds$`Perpetrator Sex`>4),]
@@ -5,7 +7,34 @@ teste2 <- crimeds[which(crimeds$`Perpetrator Age`>4),]
 teste3 <- teste2[which(crimeds$`Victim Age`>0),]
 teste3 <- teste2[which(teste2$`Victim Age`>0),]
 crimeds<- teste3
-write.csv
-help("write.csv")
-write.csv(crimeds, file= "crimeds", row.names = F)
 write.csv(crimeds, file= "crimeds.csv", row.names = F)
+
+#Describing Data 
+summary(crimeds$`Perpetrator Age`)
+summary(crimeds$`Victim Age`)
+boxplot(crimeds$`Victim Age`)
+
+##Solving the Victims Age Problem
+
+#We found some outliers here. Max value= 998? Lets clean this!
+crimedsteste <- crimeds[which(crimeds$`Victim Age` <110),]
+#Lets check if there's any age above 110
+sum(crimedsteste$`Victim Age` >110)
+#or 99
+sum(crimedsteste$`Victim Age` >99)
+#Nop. Lets continue
+#35318-35278 = we lost 40 outliers (victims with >110 years old)
+crimeds <- crimedsteste
+boxplot(crimeds)
+
+#some plots about age 
+VA <- crimeds$`Victim Age`
+PA <- crimeds$`Perpetrator Age`
+pairs(~VA+PA, data=crimeds, main="VA vs PA")
+
+install.packages("scatterplot3d")
+library(scatterplot3d)
+attach(crimeds)
+scatterplot3d(PA, VA, main="3D representation of PA vs VA")
+
+
