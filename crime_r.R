86f02cd1acd7128ebe649e39559d93f7fc2ce71e
AlexGomesDS <Alexsantogomes@gmail.com>
2017-10-06 23:19:45 +0100

    [UPD] - Removed victims and Perpretators with age over 90

--- crime_r.R
+++ crime_r.R
@@ -6,11 +6,8 @@ database <- read_excel("~/GitHub/Kaggle_cp/crimeds.xls")
 #Process of subsecting data, based on values of 3 variables:
 
 crimeds <- database[which(database$'Victim Count' > 0),]
-teste2 <- crimeds[which(crimeds$`Perpetrator Sex`>4)]
-teste2 <- crimeds[which(crimeds$`Perpetrator Sex`>4),]
-teste2 <- crimeds[which(crimeds$`Perpetrator Age`>4),]
-teste3 <- teste2[which(crimeds$`Victim Age`>0),]
-teste3 <- teste2[which(teste2$`Victim Age`>0),]
+teste2 <- crimeds[which(crimeds$`Perpetrator Age`>4 & crimeds$`Perpetrator Age`<90),]
+teste3 <- teste2[which(teste2$`Victim Age`>0 & crimeds$`Victim Age`<90),]
 crimeds<- teste3
 write.csv(crimeds, file= "crimeds.csv", row.names = F)
 
